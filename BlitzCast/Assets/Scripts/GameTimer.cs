44b48661e459515448861e6d4d2a38f170bf886a
Trinity Chung <trinity@TrinityiMac.local>
2019-04-02 15:29:24 -0700

    Add basic script for game timer
    Not have in GameManager anymore

---
+++ BlitzCast/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public Text text; 
+    private float elapsedTime;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        text.text = Mathf.Round(elapsedTime).ToString();
+    }
+}
